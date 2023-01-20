4d7944c594c262664cfd26f774d291c60631371c
David <davidmonizbusiness@outlook.pt>
2023-01-20 15:59:08 +0100

    Add project files.

---
+++ Auth1/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using FITNESSGYM.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Protocols;
+using System.Data;
+using System.Diagnostics;
+
+namespace FITNESSGYM.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Admin,Coach")]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        [AllowAnonymous]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Auth1/Program.cs
@@ -0,0 +1,54 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.EntityFrameworkCore;
+using FITNESSGYM.Data;
+using Microsoft.AspNetCore.Authorization;
+using NuGet.Protocol.Plugins;
+using System.Security.Principal;
+using FITNESSGYM.Models;
+using Microsoft.Extensions.DependencyInjection;
+
+//# Get Environment Variable
+var apiKey = Environment.GetEnvironmentVariable("SENDGRID_API_KEY");
+
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("Auth1ContextConnection") ?? throw new InvalidOperationException("Connection string 'Auth1ContextConnection' not found.");
+
+builder.Services.AddDbContext<Auth1Context>(options => options.UseSqlServer(connectionString));
+
+
+builder.Services.AddDefaultIdentity<IdentityUser>()
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<Auth1Context>();
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+app.Run();
