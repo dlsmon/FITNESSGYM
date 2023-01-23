8be4afe462ac4d9720e39427d8384dcf3f018ef7
Daikav <118259434+Daikav@users.noreply.github.com>
2023-01-23 11:30:53 +0100

    Add my account controller and view

---
+++ Auth1/Controllers/MyAccountController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FITNESSGYM.Controllers
+{
+    public class MyAccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult MyInformation() // Show details
+        {
+            return View();
+        }
+
+        public IActionResult ChangePassword()
+        {
+            return View();
+        }
+
+        public IActionResult MySubscription()
+        {
+            return View();
+        }
+
+        public IActionResult MyReservations()   //MyReservation = my planning
+        {
+            return View();
+        }
+
+        public IActionResult MyFavorites()
+        {
+            return View();
+        }
+
+        public IActionResult MyGoal()
+        {
+            return View();
+        }
+
+        public IActionResult MyStatGraph()
+        {
+            return View();
+        }
+    }
+}
