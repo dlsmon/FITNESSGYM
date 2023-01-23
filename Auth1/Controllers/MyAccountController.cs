d4886f3fbd6f27fc1edc6d8e5c5d421e016b7297
Daikav <118259434+Daikav@users.noreply.github.com>
2023-01-23 15:31:20 +0100

    add fonctionnality

--- Auth1/Controllers/MyAccountController.cs
+++ Auth1/Controllers/MyAccountController.cs
@@ -1,47 +1,76 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FITNESSGYM.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace FITNESSGYM.Controllers
 {
     public class MyAccountController : Controller
     {
-        public IActionResult Index()
+        private readonly FITNESSGYMDBContext _context;
+
+        public MyAccountController(FITNESSGYMDBContext context)
         {
-            return View();
+            _context = context;
         }
 
-        public IActionResult MyInformation() // Show details
+        [Authorize]
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var client = await _context.Client.FirstAsync(m => m.IdUser == User.Identity.Name);
+
+            return View(client);
         }
 
-        public IActionResult ChangePassword()
+        //must be logged in
+        [Authorize]
+        public async Task<IActionResult> MyInformation() // Show details
         {
-            return View();
+            var client = await _context.Client.FirstAsync(m => m.IdUser == User.Identity.Name);
+                
+            return View(client);
         }
 
+        [Authorize]
+        public async Task<IActionResult> ChangePassword()
+        {
+            var client = await _context.Client.FirstAsync(m => m.IdUser == User.Identity.Name);
+
+            return View(client);
+        }
+
+        [Authorize]
         public IActionResult MySubscription()
         {
             return View();
         }
 
+        [Authorize]
         public IActionResult MyReservations()   //MyReservation = my planning
         {
             return View();
         }
 
-        public IActionResult MyFavorites()
+        [Authorize]
+        public IActionResult MyFavourites()
         {
             return View();
         }
 
-        public IActionResult MyGoal()
+        [Authorize]
+        public async Task<IActionResult> MyGoal()
         {
-            return View();
+            var client = await _context.Client.FirstAsync(m => m.IdUser == User.Identity.Name);
+
+            return View(client);
         }
 
-        public IActionResult MyStatGraph()
+        [Authorize]
+        public async Task<IActionResult> MyStatGraph()
         {
-            return View();
+            var client = await _context.Client.FirstAsync(m => m.IdUser == User.Identity.Name);
+
+            return View(client);
         }
     }
 }
